7347acfa7c6824de5a1cd958bf9ec41f51978570
Mohit Wadhwa <mohit3wadhwa@gmail.com>
2020-08-08 12:17:27 +0100

    v1.0

--- discordBot.py
+++ discordBot.py
@@ -18,38 +18,69 @@ print(f"yahan pe --> ", TOKEN)
 
 bot = commands.Bot(command_prefix='!')
 
+@bot.command(name='gn', help="This command greets good night")
+async def gn(ctx):
+    userName = str(ctx.message.author)
+    formattedName, _ = userName.split("#")
+    response = "Sleep tight, " + formattedName + " :)"
+    await ctx.send(response)
+
+@bot.command(name='gm', help="This command greets good morning")
+async def gm(ctx):
+    userName = str(ctx.message.author)
+    formattedName, _ = userName.split("#")
+    response = "A very goooood morning... " + formattedName + " :)"
+    await ctx.send(response)
+
+@bot.command(name='hello', help="This command greets hello")
+async def hello(ctx):
+    userName = str(ctx.message.author)
+    formattedName, _ = userName.split("#")
+    response = "Hello, you dirty fellow " + formattedName + "! How are you?"
+    await ctx.send(response)
+
+@bot.command(name='hi', help="This command greets hello")
+async def hi(ctx):
+    userName = str(ctx.message.author)
+    formattedName, _ = userName.split("#")
+    response = "Hello, you dirty fellow " + formattedName + ". Hope you are doing awesome!"
+    await ctx.send(response)
 
 @bot.command(name='birth', help="This command checks whether anyone has birthday today!")
 async def birthday_check(ctx):
-    response = "Today is no one's Birthday"
+    userName = str(ctx.message.author)
+    formattedName, _ = userName.split("#")
+    response = "No one has a Birthday today, " + formattedName + " !"
     await ctx.send(response)
 
-
-@bot.command(name='roll_dice', help="This command rolls a dice and shows a number between '1-6'")
+@bot.command(name='dice', help="This command rolls a dice and shows a number between '1-6'")
 async def rollDice(ctx):
     number = random.randint(1, 6)
     await ctx.send(number)
 
+@bot.command(name='add', help="This command adds two numbers. Provide '!add' followed by 2 numbers with space")
+async def add(ctx, firstNum: int, secondNum: int):
+    number = firstNum + secondNum
+    await ctx.send(number)
+
+@bot.command(name='sub', help="This command subtracts two numbers. Provide '!sub' followed by 2 numbers with space")
+async def sub(ctx, firstNum: int, secondNum: int):
+    number = firstNum - secondNum
+    await ctx.send(number)
+
+@bot.command(name='mul', help="This command multiplies two numbers. Provide '!mul' followed by 2 numbers with space")
+async def mul(ctx, firstNum: int, secondNum: int):
+    number = firstNum * secondNum
+    await ctx.send(number)
+
+@bot.command(name='div', help="This command divices one num from another. Provide '!div' followed by 2 numbers with space")
+async def div(ctx, firstNum: int, secondNum: int):
+    number = firstNum / secondNum
+    await ctx.send(float(number))
 
 @bot.event
 async def on_command_error(ctx, error):
-    await ctx.send('No such command, try "!help"')
-
+    await ctx.send('No such command, try "!help" for more info ...')
 
 
 bot.run(TOKEN)
-
-# async def on_ready():
-#     for guild in client.guilds:
-#         if guild.name == GUILD:
-#             break
-
-#     print(
-#         f'{client.user} is connected to the following guild:\n'
-#         f'{guild.name}(id: {guild.id})'
-#     )
-
-#     members = '\n - '.join([member.name for member in guild.members])
-#     print(f'Guild Members:\n - {members}')
-#     print(type(members))
-# client.run(TOKEN)
